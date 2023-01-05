ffcbeda037ebf51fe5b95709a8c6e4b3cbf8d504
Vlad Tsibizov <vladtsibizov@gmail.com>
2023-01-05 03:37:31 +0300

    remove second dotenv.config

--- src/middleware/authMiddleware/index.ts
+++ src/middleware/authMiddleware/index.ts
@@ -1,12 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
-import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 import { errorResponse } from '#shared/responses'
 import { User } from '#models'
 
-
-dotenv.config()
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const { _id } = req.session
