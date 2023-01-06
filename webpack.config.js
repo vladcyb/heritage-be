2d932816a5bcd044bc3ba76562fabb2c1caca949
Vlad Tsibizov <vladtsibizov@gmail.com>
2023-01-06 04:15:35 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const webpackNodeExternals = require('webpack-node-externals')
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+module.exports = {
+  target: 'node',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: ['.', 'node_modules'],
+    extensions: ['.ts', '.js', '...'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  externals: [webpackNodeExternals()],
+}
