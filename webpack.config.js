a1b32be6c396516ff2b857a4649f6e32eebb82bd
Vlad Tsibizov <vladtsibizov@yandex.ru>
2023-01-07 21:56:50 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   externals: [webpackNodeExternals()],
   watch: true,
